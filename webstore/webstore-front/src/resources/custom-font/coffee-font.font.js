f2f9cdcc4f287f528fbe920831a54359f406996c
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2020-01-31 14:23:54 +0100

    Fork of my CS50 repo

--- webstore/webstore-front/src/resources/custom-font/coffee-font.font.js
+++ webstore/webstore-front/src/resources/custom-font/coffee-font.font.js
@@ -2,7 +2,7 @@ module.exports = {
   'files': [
     './original/processed/*.svg'
   ],
-  'fontName': 'ingredient-font',
+  'fontName': 'coffee-font',
   'classPrefix': 'icon-',
   'baseSelector': '.icon',
   'types': ['eot', 'woff', 'woff2', 'ttf', 'svg'],
